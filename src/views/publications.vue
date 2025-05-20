5fea591dd1746abf30b7a590bec31c08f40a6e7a
jacknichao <804030146@qq.com>
2025-05-20 21:04:00 +0800

    add asej-2025 Semi-Supervised Software Vulnerability Assessment via Code Lexical and Structural Information Fusion

--- src/views/publications.vue
+++ src/views/publications.vue
@@ -5,7 +5,7 @@
 <h2 >Publication Summary</h2>
 <ul>
 	<li>
-		<b>Journal Papers</b>: &emsp;IEEE Transactions on Software Engineering(TSE) * 4, &emsp; ACM Transactions on Software Engineering and Methodology(TOSEM) * 4,  &emsp;Journal of Systems and Software(JSS) *2.&emsp; Information and Software Technology(IST) * 2, &emsp; Journal of Software: Evolution and Process(JSEP) * 2; &emsp; Journal of Computer Science and Technology(JCST) * 1.
+		<b>Journal Papers</b>: &emsp;IEEE Transactions on Software Engineering(TSE) * 4, &emsp; ACM Transactions on Software Engineering and Methodology(TOSEM) * 4,  &emsp;Journal of Systems and Software(JSS) *2.&emsp; Information and Software Technology(IST) * 2, &emsp; Journal of Software: Evolution and Process(JSEP) * 2; &emsp; Journal of Computer Science and Technology(JCST) * 1; &emsp;Automated Software Engineering Journal(ASEJ) * 1.
 	</li>
 	<li>
 	<b>Conference Papers</b>: &emsp;FSE * 4, &emsp;ASE * 2, &emsp;ISSTA * 2,&emsp;ICSE * 1, &emsp;SANER * 1,&emsp;ISSRE * 1,&emsp;ICPC * 2,&emsp;MSR * 2,&emsp;COMPSAC * 1,&emsp; Internetware * 1,&emsp;SEKE * 1,&emsp;APSEC * 1.
@@ -73,6 +73,18 @@ export default {
 			{
 					year: "2025",
 					items: [
+					{
+							id: "J16",
+							short: "ASEJ",
+							long: confs["ASEJ"],
+							title: "Semi-Supervised Software Vulnerability Assessment via Code Lexical and Structural Information Fusion",
+							authors: ["Xiang Chen", "Wenlong Pei", "Yiling Huang", "Guilong Lu", "Yong Liu", "Chao Ni"],
+ 							corresponding_authors: [""],
+							pdf: "",
+							doi: "",
+							CCF_grade: "(CCF-B)",
+							award_winning: false
+						},
 					{
 							id: "J15",
 							short: "TOSEM",
