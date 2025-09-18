8fa7509a77b28548a396fe99191135320cfa186f
jacknichao <804030146@qq.com>
2025-09-18 23:36:33 +0800

    adding: emse-2025 Learning-based Models for Vulnerability Detection: An Extensive Study

--- src/views/publications.vue
+++ src/views/publications.vue
@@ -5,7 +5,9 @@
 <h2 >Publication Summary</h2>
 <ul>
 	<li>
-		<b>Journal Papers</b>: &emsp;IEEE Transactions on Software Engineering(TSE) * 5, &emsp; ACM Transactions on Software Engineering and Methodology(TOSEM) * 4,  &emsp;Journal of Systems and Software(JSS) *2.&emsp; Information and Software Technology(IST) * 2, &emsp; Journal of Software: Evolution and Process(JSEP) * 2; &emsp; Journal of Computer Science and Technology(JCST) * 1; &emsp;Automated Software Engineering Journal(ASEJ) * 1.
+		<b>Journal Papers</b>: &emsp;IEEE Transactions on Software Engineering(TSE) * 5, &emsp; ACM Transactions on Software Engineering and Methodology(TOSEM) * 4,  &emsp;
+		Empirical Software Engineering(EMSE) * 1, &emsp;
+		Journal of Systems and Software(JSS) *2.&emsp; Information and Software Technology(IST) * 2, &emsp; Journal of Software: Evolution and Process(JSEP) * 2; &emsp; Journal of Computer Science and Technology(JCST) * 1; &emsp;Automated Software Engineering Journal(ASEJ) * 1.
 	</li>
 	<li>
 	<b>Conference Papers</b>: &emsp;FSE * 4, &emsp;ASE * 2, &emsp;ISSTA * 2,&emsp;ICSE * 1, &emsp;SANER * 1,&emsp;ISSRE * 2,&emsp;ICPC * 2,&emsp;MSR * 2,&emsp;COMPSAC * 1,&emsp; Internetware * 1,&emsp;SEKE * 1,&emsp;APSEC * 1.
@@ -73,6 +75,18 @@ export default {
 			{
 					year: "2025",
 					items: [
+							{
+							id: "J18",
+							short: "EMSE",
+							long: confs["EMSE"],
+							title: "Learning-based Models for Vulnerability Detection: An Extensive Study",
+							authors: ["Chao Ni", "Xin Yin", "Liyu Shen", "Shaohua Wang"],
+ 							corresponding_authors: ["Chao Ni"],
+							pdf: "",
+							doi: "",
+							CCF_grade: "(CCF-B)",
+							award_winning: false
+						},
 										{
 							id: "J17",
 							short: "TSE",
